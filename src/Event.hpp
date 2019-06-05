af1511ffa307de14093d7f6da717974d2a95e8a5
Louis Mayor <lmayor@REDKITEGAMES.local>
2019-06-05 12:52:52 +0100

    renamed ugly template names

--- src/Event.hpp
+++ src/Event.hpp
@@ -6,58 +6,58 @@
 
 /* Wanting to create an extremely simple event system. Design based on Unity Actions */
 
-template<typename _pTy>
+template<typename _paramT>
 class Event
 {
 public:
-	Event( ) = default;
+	Event() = default;
 
-	Event( const Event& other ) = delete;
-	Event operator=( const Event& other ) = delete;
+	Event(const Event& other) = delete;
+	Event operator=(const Event& other) = delete;
 
-	template<typename _ptrFunc>
-	void operator+=( _ptrFunc func )
+	template<typename _funcPointer>
+	void operator+=(_funcPointer func)
 	{
-		_funcs.push_back( std::function<void( _pTy )>( func ) );
+		_funcs.push_back(std::function<void(_paramT)>(func));
 	}
 
-	template<typename _ptrFunc>
-	void operator-=( _ptrFunc func )
+	template<typename _funcPointer>
+	void operator-=(_funcPointer func)
 	{
-		std::function<void( _pTy )> to_find( func );
-		size_t addr_to_find = get_addr( to_find );
+		std::function<void(_paramT)> to_find(func);
+		size_t addr_to_find = get_addr(to_find);
 
-		std::vector <std::vector<std::function < void( _pTy )> >::iterator> to_remove;
+		std::vector <std::vector<std::function < void(_paramT)> >::iterator> to_remove;
 
-		for( auto it = _funcs.begin( ); it != _funcs.end( ); ++it )
+		for (auto it = _funcs.begin(); it != _funcs.end(); ++it)
 		{
-			if( addr_to_find == get_addr( *it ) )
+			if (addr_to_find == get_addr(*it))
 			{
-				to_remove.push_back( it );
+				to_remove.push_back(it);
 			}
 		}
 
-		for (auto &i : to_remove)
+		for (auto& i : to_remove)
 		{
-			_funcs.erase( i );
+			_funcs.erase(i);
 		}
 	}
 
-	void invoke_safe( _pTy value )
+	void invoke_safe(_paramT value)
 	{
-		for( auto &func : _funcs )
+		for (auto& func : _funcs)
 		{
-			func( value );
+			func(value);
 		}
 	}
 private:
-	std::vector<std::function<void( _pTy )>> _funcs;
+	std::vector<std::function<void(_paramT)>> _funcs;
 
 	// src: https://stackoverflow.com/a/18039824
-	size_t get_addr( std::function < void( _pTy ) > f )
+	size_t get_addr(std::function < void(_paramT) > f)
 	{
-		typedef void( fnType )( _pTy );
-		fnType ** fnPointer = f.template target<fnType*>( );
+		typedef void(fnType)(_paramT);
+		fnType** fnPointer = f.template target<fnType*>();
 		return reinterpret_cast<size_t> (*fnPointer);
 	}
 };
